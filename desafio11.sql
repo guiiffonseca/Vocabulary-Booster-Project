8a50fb4deb81ae651c28ac9ab14cdf0ff1ca375a
giiffonseca <guii.ffonseca22@gmail.com>
2021-10-29 14:52:38 -0300

    Req 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,5 @@ COUNT(T1.ContactName) - 1 AS `Número de compatriotas`
 FROM w3schools.customers AS T1
 JOIN w3schools.customers AS T2 ON T1.Country = T2.Country
 GROUP BY T1.ContactName, T1.Country
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
