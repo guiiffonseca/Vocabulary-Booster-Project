4f954e7221844be72d7f434b8b13d6f43933b104
giiffonseca <guii.ffonseca22@gmail.com>
2021-10-29 15:38:10 -0300

    desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,5 +1,6 @@
 SELECT 	CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo funcionário 1`,
 E.SALARY AS `Salário funcionário 1`,
+E.PHONE_NUMBER AS `Telefone funcionário 1`,
 CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS `Nome completo funcionário 2`,
 E2.SALARY AS `Salário funcionário 2`,
 E2.PHONE_NUMBER AS `Telefone funcionário 2`
